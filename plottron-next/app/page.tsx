a6d50be1fa5a0dd06c5b814f2f1b5c5c66c157c5
dottapper <mavon@dottapper.com>
2025-08-04 11:54:32 +0900

    Fix various build and configuration issues

--- plottron-next/app/page.tsx
+++ plottron-next/app/page.tsx
@@ -1,7 +1,7 @@
-import Header from '@/components/Header'
-import InfiniteCanvas from '@/components/InfiniteCanvas'
-import { canvasData } from '@/data/canvasData'
-import '@/styles/infinite-canvas.css'
+import Header from '../components/Header'
+import InfiniteCanvas from '../components/InfiniteCanvas'
+import { canvasData } from '../data/canvasData'
+import '../styles/infinite-canvas.css'
 
 export default function Home() {
   return (
